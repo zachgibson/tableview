252bc46a50977644a47895c7eb9262af0bd61f0c
zachgibson <zackgib@aol.com>
2021-02-05 16:11:42 -0600

    Adds numberOfSections method and registers cell in init

--- ios/TableViewController.swift
+++ ios/TableViewController.swift
@@ -11,16 +11,12 @@ import UIKit
 class TableViewController: UITableViewController {
   override init(style: UITableView.Style) {
     super.init(style: style)
+    
+    tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
   }
   
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
-  }
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-
-    tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
   }
   
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -31,5 +27,9 @@ class TableViewController: UITableViewController {
 
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return 10
+  }
+  
+  override func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
   }
 }
