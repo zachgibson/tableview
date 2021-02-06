6710c99c17979c633c7aebe985e713d8b249ea44
zachgibson <zackgib@aol.com>
2021-02-06 09:11:45 -0600

    Fixes tableview cells not rendering

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import {requireNativeComponent} from 'react-native';
+import {Dimensions, requireNativeComponent} from 'react-native';
 
 const TableView =
   global['TableView'] ||
   (global['TableView'] = requireNativeComponent('TableView'));
 
 const App: () => React$Node = () => (
-  <TableView style={{width: 375, height: 800}} />
+  <TableView
+    style={{
+      flex: 1,
+    }}
+  />
 );
 
 export default App;
--- ios/TableView.swift
+++ ios/TableView.swift
@@ -9,11 +9,33 @@ import Foundation
 import React
 
 @objc(TableView)
-class TableView: RCTViewManager {
+class TableView: RCTViewManager, UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+      return 10
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+      let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+      cell.textLabel?.text = "Yeet"
+      return cell
+  }
+
   override func view() -> UIView! {
-    let tableViewController = TableViewController(style: .plain)
-    tableViewController.tableView.reloadData()
-    return tableViewController.tableView
+    let view = UIView()
+    
+    let tableView = UITableView()
+    tableView.register(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
+    tableView.dataSource = self
+    view.addSubview(tableView)
+    tableView.translatesAutoresizingMaskIntoConstraints = false
+    tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+    tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+    tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+    
+    view.addSubview(tableView)
+    
+    return view
   }
   
   override class func requiresMainQueueSetup() -> Bool {
